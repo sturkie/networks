5f17225633951a4dc00cc98f7e5084a637a1f1dc
sarah sturkie <sstur002@ucr.edu>
2020-11-05 23:13:39 -0800

    added timeout

--- rdt3.0/client.py
+++ rdt3.0/client.py
@@ -3,6 +3,8 @@
 import socket    #for sockets
 import sys    #for exit
 from check import ip_checksum
+import time
+
 
 # create dgram udp socket
 try:
@@ -13,12 +15,26 @@ except socket.error:
 
 host = 'localhost';
 port = 8888;
+addr = (host,port)
+
+s.settimeout(1)
 
 rcvpkt = []
+seq = 0
 ACK = 'ACK'
+
+def isACK(rcvpkt,seqnum):
+    if ACK == 'ACK' and seqnum == 0:
+        seq = 1
+        print 'Received ACK for seq0'
+        return True
+    elif ACK == 'ACK' and seqnum == 1:
+        print 'Received ACK for seq1'
+        return True
 
 def isNAK(rcvpkt):
     if ACK == 'NAK':
+        print 'Received NAK'
         return True
     else:
         return False
@@ -27,16 +43,16 @@ def isNAK(rcvpkt):
     
 def rdt_rcv(rcvpkt):
     #here we will receive information back from the server regarding the status of the information delievered
-    rcvpkt = make_pkt(s.recvfrom(1024)) #update rcvpkt
     
-    d = rcvpkt[0]
-    
-    
-    if 'NAK for ' in d:
+    if 'NAK' in rcvpkt[0]:
+        #print 'Received NAK'
         ACK = 'NAK'
-    elif 'ACK for ' in d:
+        return True
+    elif 'ACK' in rcvpkt[0]:
+        #print 'Rceived ACK'
         ACK = 'ACK'
     
+    d = rcvpkt[0]
     
     reply = d[0]
     addr = d[1]
@@ -47,19 +63,30 @@ def rdt_rcv(rcvpkt):
     return True
     
 
-def make_pkt(data):
+def make_pkt(seq,data,checksum):
     #let packet be an array containing: data
+    #calculate checksum here
+    #str = ''.join(data)
+    
     packet = []
+    packet.append(seq)
     packet.append(data)
+    packet.append(checksum)
     return packet
 
 def udt_send(sndpkt):
     try :
-        str = ''.join(sndpkt)
+        #seq = ''.join(sndpkt[0])
+        seq = sndpkt[0]
+        msg = ''.join(sndpkt[1])
+        
         #print 'This is str: ' + str
-        checksum = ip_checksum(str)
+        #checksum = ip_checksum(str)
         #Set the whole string
-        s.sendto(str + ';' + checksum, (host, port))
+        #print 'Sending: ' + str(seq) + '|' + msg + ';' + sndpkt[2]
+        s.sendto(str(seq) + '|' + msg + ';' + sndpkt[2], (host, port))
+        
+        #s.sendto(sndpkt,(host,port))
         
     
     except socket.error, msg:
@@ -68,19 +95,65 @@ def udt_send(sndpkt):
     
 #wait for call from above
 def rdt_send(data):
+    seq = 0
+    #compute checksum
+    checksum = ip_checksum(data) #might have to make to string
     #make packet
-    sndpkt = make_pkt(data)
+    sndpkt = make_pkt(0,data,checksum)
     #send packet
     udt_send(sndpkt)
     
-    rdt_rcv(rcvpkt)
-    if rdt_rcv(rcvpkt) and isNAK(rcvpkt):
-        #send again
-        print 'NAK received. Resending...'
+    try:
+        #print 'About to receive input'
+        d = s.recvfrom(1024) #update rcvpkt
+        #print 'Input is: ' + d[0]
+        
+        rcvpkt.append(d)
+        
+        rdt_rcv(rcvpkt)
+        rcvpkt.pop()
+    except timeout:
+        print 'Timeout'
+        #resent
         udt_send(sndpkt)
+    #rdt_rcv(rcvpkt)
+    
+    
+    if isNAK(rcvpkt):
+        #send again
+        #print 'NAK received. Resending...'
+        udt_send(sndpkt)
+        del sndpkt[:]
+    elif isACK(rcvpkt,0):
+        #send again but with seq1
+        #print 'ACK received. Sending sequence 1'
+        sndpkt = make_pkt(1, data, checksum)
+        udt_send(sndpkt)
+        del sndpkt[:]
+        #will receive second ACK
+        
+        try:
+            rcvpkt.append(s.recvfrom(1024))
+            rdt_rcv(rcvpkt)
+            rcvpkt.pop()
+            
+            if isACK(rcvpkt, 1):
+                try:
+                    rcvpkt.append(s.recvfrom(1024))
+                    #print 'Receiving: ' + str(rcvpkt[0])
+                    rdt_rcv(rcvpkt)
+                    rcvpkt.pop()
+                except timeout:
+                    print 'Timeout'
+                    #resend
+        except timeout:
+            sndpkt = make_pkt(1, data, checksum)
+            udt_send(sndpkt)
+            del sndpkt[:]
+            print 'Timeout'
     #else do nothing except wait
 
 while(1) :
     msg = raw_input('Enter message to send : ')
     rdt_send(msg)
-    #rdt_rcv(rcvpkt)
+    
--- rdt3.0/server.py
+++ rdt3.0/server.py
@@ -7,92 +7,136 @@ from check import ip_checksum
 HOST = ''    # Symbolic name meaning all available interfaces
 PORT = 8888    # Arbitrary non-privileged port
 
+
 # Datagram (udp) socket
 try :
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     print 'Socket created'
-except socket.error, msg :
-    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+except socket.error, errormsg :
+    print 'Failed to create socket. Error Code : ' + str(errormsg[0]) + ' Message ' + errormsg[1]
     sys.exit()
 
 
 # Bind socket to local host and port
 try:
     s.bind((HOST, PORT))
-except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+except socket.error , errormsg:
+    print 'Bind failed. Error Code : ' + str(errormsg[0]) + ' Message ' + errormsg[1]
     sys.exit()
     
 print 'Socket bind complete'
 
 
 packet = []
+rcvpkt = []
 
-
-def udt_send(ACK):
-    if ACK == 'ACK':
-        s.sendto('ACK for ' + data, addr)
-    elif ACK == 'NAK':
-        s.sendto('NAK for ' + data, addr)
+def has_seq0(rcvpkt):
+    if '0' in seq:
+        return True
+        
+        
     
-def corrupt(rcvpkt):
-    d = rcvpkt[0]
-    
-    checksum2 = ip_checksum(msg)
-    
-    #print 'Checksum = ' + checksum + ', Checksum2 = ' + checksum2
-    if str(checksum) == str(checksum2):
-        ACK = 'ACK'
-        return False
-    elif str(checksum) != str(checksum2):
-        ACK = 'NAK'
+def has_seq1(rcvpkt):
+    if '1' in seq:
         return True
         
 
+def udt_send(ACK):
+    if ACK == 'ACK':
+        print 'Sending ACK...'
+        s.sendto('ACK for ' + data, addr)
+    elif ACK == 'NAK':
+        print 'Sending NAK...'
+        s.sendto('NAK for ' + data, addr)
+    
+    return
+    
+    
+    
+def corrupt(rcvpkt):
+    
+    checksum2 = ip_checksum(msg)
+    
+    #print 'Checksum = ' + str(checksum) + ', Checksum2 = ' + checksum2
+    if str(checksum) != str(checksum2):
+        ACK = 'NAK'
+        #print 'They are NOT the same'
+        return True
+
+
+
+def notCorrupt(rcvpkt):
+    checksum2 = ip_checksum(rcvpkt[1])
+    
+    #print 'Checksum = ' + str(rcvpkt[2]) + ', Checksum2 = ' + checksum2
+    if str(rcvpkt[2]) == str(checksum2):
+        ACK = 'ACK'
+        #print 'They are the same'
+        return True
+
+
 
 def deliver_data(data):
-    reply = 'OK...' + msg
+    reply = 'OK...' + rcvpkt[1]
     
     #send to rdt_rcv
     s.sendto(reply, addr)
-    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + msg.strip()
+    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + str(rcvpkt[1])
+    del rcvpkt[:]
+
+
 
 #wait for call from below
 def rdt_rcv(packet):
+    
+    if corrupt(rcvpkt) or has_seq0(rcvpkt):
+        #print 'the message is on seq 0, sending NAK'
+        udt_send('NAK') #send again
+    elif notCorrupt(rcvpkt) and has_seq1(rcvpkt):
+        #print 'the message is clear ro reply to'
+        udt_send('ACK')
+        #extract(packet, data)
+        deliver_data(rcvpkt)
+    
+    #del rcvpkt[:]
+    return
+
+#now keep talking with the client
+while 1:
+    # receive data from client (data, addr)
     #d = s.recvfrom(1024)
-    packet.append(s.recvfrom(1024))
-    d = packet[0]
+    d = (s.recvfrom(1024))
+    #d = packet[0]
     
     global addr
     global data
     global msg
     global checksum
+    global seq
     
     myList = []
     
     data = d[0]
     addr = d[1]
-    if ';' in str(data):
+    
+    #analyze data
+    if '|' in str(data):
         #index = data.find(';')
         #msg = data[0:]
-        myList = data.split(';')
-        msg = myList[0]
-        checksum = myList[1]
+        myList = data.split('|')
+        seq = myList[0]
+        data = myList[1]
+        if ';' in str(data):
+            myList2 = data.split(';')
+            msg = myList2[0]
+            checksum = myList2[1]
     
-    if corrupt(packet):
-        udt_send('NAK')
-    else:
-        udt_send('ACK')
-        #extract(packet, data)
-        deliver_data(data)
-        packet.pop()
-
-
-
-#now keep talking with the client
-while 1:
-    # receive data from client (data, addr)
+    #print 'Sequence: ' + str(seq) + ' Message: ' + msg + ' Checksum = ' + checksum
     
+    #remake packet
+    rcvpkt.append(seq)
+    rcvpkt.append(msg)
+    rcvpkt.append(checksum)
     rdt_rcv(packet)
     
     
